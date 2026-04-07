9700371a83845c539c740538dfc7d3949d363981
nguyenvukhang <brew4k@gmail.com>
2026-04-08 00:30:57 +0800

    Add documentation for Client and Monitor

--- dtypes.h
+++ dtypes.h
@@ -73,10 +73,22 @@ struct Client {
     int x, y, w, h;
     int oldx, oldy, oldw, oldh;
     int basew, baseh, incw, inch, maxw, maxh, minw, minh, hintsvalid;
-    int bw, oldbw;
+    /* Border width. */
+    int bw;
+    /* Old border width. */
+    int oldbw;
+    /* Bitmask of active tags. */
     unsigned int tags;
-    int isfixed, isfloating, isurgent, neverfocus, oldstate, isfullscreen;
+    int isfixed;
+    int isfloating;
+    int isurgent;
+    int neverfocus;
+    /* Old floating state (previous value for `isfloating`). */
+    int oldstate;
+    int isfullscreen;
+    /* Next client in the linked list of clients. */
     Client *next;
+    /* Next client in the display stack. */
     Client *snext;
     Monitor *mon;
     Window win;
@@ -99,16 +111,38 @@ struct Monitor {
     float mfact;
     int nmaster;
     int num;
-    int by;             /* bar geometry */
-    int mx, my, mw, mh; /* screen size */
-    int wx, wy, ww, wh; /* window area  */
+    /* Bar geometry. */
+    int by;
+    /* Screen size: x-coordinate. */
+    int mx;
+    /* Screen size: y-coordinate. */
+    int my;
+    /* Screen size: width. */
+    int mw;
+    /* Screen size: height. */
+    int mh;
+    /* Window area: x-coordinate. */
+    int wx;
+    /* Window area: y-coordinate. */
+    int wy;
+    /* Window area: width. */
+    int ww;
+    /* Window area: height. */
+    int wh;
+    /* Index of selected tags. */
     unsigned int seltags;
+    /* Index of selected layout. */
     unsigned int sellt;
     unsigned int tagset[2];
+    /* 0 means no bar. */
     int showbar;
+    /* 0 means bottom bar. */
     int topbar;
+    /* Linked list of clients. */
     Client *clients;
+    /* Selected client. */
     Client *sel;
+    /* Clients ordered by stack. */
     Client *stack;
     Monitor *next;
     Window barwin;

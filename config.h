dcce7e35e52d7653b286306b11ffd8ff2cea8061
nguyenvukhang <brew4k@gmail.com>
2026-09-06 11:35:18 +0800

    Use kitty as terminal

--- config.h
+++ config.h
@@ -71,7 +71,7 @@ static const Layout LAYOUTS[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "rofi", "-show", "run", "-monitor", dmenumon, "-matching", "fuzzy", "-sort", "-sorting-method", "fzf", NULL };
-static const char *termcmd[]  = { "alatty", NULL };
+static const char *termcmd[]  = { "kitty", NULL };
 
 static const Key KEYS[] = {
 	/* modifier                     key        function        argument */
